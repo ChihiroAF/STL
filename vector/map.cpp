a7cb8308dfd52e7faf2628c5203d0ac67127915a
QQX <809257901@qq.com>
2017-08-23 23:56:03 +0800

    3

--- vector/map.cpp
+++ vector/map.cpp
@@ -18,46 +18,91 @@ struct print {
 		printf_s("%d", m);
 		printf_s(" ");
 	}
-	
+	void operator()(const std::map<int, std::string>::value_type &pair)
+	{
+		printf_s("%d:", pair.first);
+		printf_s("%s\n", pair.second.c_str());
+	}
+
 };
 
-
+/*
 int main() {
-	std::map <int, std::string> m_string;
+	std::map <int, std::string> m_string1;
+	std::map <int, std::string> m_string2;
 	std::map <int, int> m_int;
 	std::map <int, std::string>::iterator m_str_iter;
 
 	for (int i = 0; i < 50;i++) {
-		m_string.insert(std::pair<int,std::string> (i, CName().GetName()));
+		m_string1.insert(std::pair<int,std::string> (i, CName().GetName()));
 		Sleep(1);
 	}
 
-	for (m_str_iter = m_string.begin(); m_str_iter != m_string.end(); m_str_iter++) {
+	for (m_str_iter = m_string1.begin(); m_str_iter != m_string1.end(); m_str_iter++) {
 		std::cout << m_str_iter->first << ":"<< m_str_iter->second << std::endl;
 	}
-	std::cout << "长度为:" << m_string.size() << std::endl;
-	m_string.clear();
-	std::cout << "clear后长度为:" << m_string.size() << std::endl;
+	std::cout << "长度为:" << m_string1.size() << std::endl;
+	//m_string1.clear();
+	std::cout << "clear后长度为:" << m_string1.size() << std::endl;
 
-
-	srand((unsigned)time(NULL));
-	for (int i = 0; i < 20; i++) {
-		int key = (rand() % (100 - 20)) + 20;
-		m_string.insert(std::pair<int, std::string>(key, CName().GetName()));
-		Sleep(key);
-	}
-	for (m_str_iter = m_string.begin(); m_str_iter != m_string.end(); m_str_iter++) {
-		std::cout << m_str_iter->first << ":" << m_str_iter->second << std::endl;
-	}
-	std::cout << "长度为:" << m_string.size() << std::endl;
 
 	//在睡眠时间较短的时候，长度基本固定，当采取睡眠时间不确定来规避rand的取值一定，这样每次
-	//可能重复的key个数不一定，而map不允许重复的值，所以每次map的长度也就不一致，而且打印出来
+	//可能重复的key个数不一定，而map不允许重复的key值，所以每次map的长度也就不一致，而且打印出来
 	//的结果也是按照key的大小排序的
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 50; i++) {
+		int key = (rand() % (100 - 20)) + 20;
+		m_string2.insert(std::pair<int, std::string>(key, CName().GetName()));
+		Sleep(key);
+	}
+	//for循环打印
+	SYSTEMTIME start;
+	GetLocalTime(&start);
+	for (m_str_iter = m_string2.begin(); m_str_iter != m_string2.end(); m_str_iter++) {
+		std::cout << m_str_iter->first << ":" << m_str_iter->second << std::endl;
+	}
+	SYSTEMTIME end;
+	GetLocalTime(&end);
+	std::cout << "长度为:" << m_string2.size() << std::endl;
+	std::cout << "时间为:" << end.wMilliseconds-start.wMilliseconds << std::endl;
+	
+	//自定义的打印函数打印,这个打印比较快
+	SYSTEMTIME start1;
+	GetLocalTime(&start1);
+	std::for_each(m_string2.cbegin(), m_string2.cend(), print());
+	SYSTEMTIME end1;
+	GetLocalTime(&end1);
+	std::cout << "时间为:" << end1.wMilliseconds - start1.wMilliseconds << std::endl;
+	
+	//使用迭代器寻找两个map是否存在相同的value
+	int count = 0;
+	for (m_str_iter = m_string1.begin(); m_str_iter != m_string1.end(); m_str_iter++) {
+		std::map <int, std::string>::iterator find_map;
+		for (find_map = m_string2.begin(); find_map != m_string2.end(); find_map++) {
+			if (find_map->second == m_str_iter->second) {
+				count++;
+			}
+		}
 
-	//std::for_each(m_string.cbegin(), m_string.cend(), print());
+	}
+	std::cout << "两个map重复个数为:" << count << std::endl;
+
+	//使用find函数查找40-50的key值
+	std::map <int, std::string>::iterator Appear;
+	for (int i = 40; i < 50;i++) {
+		Appear = m_string2.find(i);
+		if (Appear != m_string2.end()) {
+			std::cout << "Find, the value is " << Appear->second << std::endl;
+		}
+	}
+
+	//swap交换两个的值
+	std::swap(m_string1,m_string2);
+	std::for_each(m_string1.cbegin(), m_string1.cend(), print());
+	std::for_each(m_string2.cbegin(), m_string2.cend(), print());
 
 
 	system("pause");
 	return 0;
 }
+*/
---
+++ vector/set.cpp
@@ -0,0 +1,80 @@
+#include "set"
+#include "map"
+#include <iostream>
+#include "name.h"
+#include "windows.h"
+#include "string"
+#include "algorithm"
+# include <stdio.h>
+#include <time.h>  
+
+struct print {
+	void operator()(std::string x)
+	{
+		printf_s(x.c_str());
+		printf_s("\n");
+	}
+	void operator()(int m)
+	{
+		printf_s("%d", m);
+		printf_s(" ");
+	}
+	void operator()(const std::map<int, std::string>::value_type &pair)
+	{
+		printf_s("%d:", pair.first);
+		printf_s("%s\n", pair.second.c_str());
+	}
+
+};
+
+int main() {
+
+	std::set<std::string> set_str;
+	std::set<std::string> set_str2;
+	std::set<std::string>::iterator m_str_iter;
+	std::set<std::string>::reverse_iterator m_str_iter1;
+
+	for (int i = 0; i < 50; i++) {
+		set_str.insert(CName().GetName());
+		Sleep(1);
+	}
+	std::cout << "set_str为:" <<  std::endl;
+	for (m_str_iter = set_str.begin(); m_str_iter != set_str.end(); m_str_iter++) {
+		std::cout << *m_str_iter<< std::endl;
+	}
+
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 50; i++) {
+		int key = (rand() % (100 - 20)) + 20;
+		set_str2.insert(CName().GetName());
+		Sleep(key);
+	}
+	std::cout << "set_str2为:" << std::endl;
+	std::for_each(set_str2.cbegin(), set_str2.cend(), print());
+
+	std::cout << "set_str长度为:" << set_str.size() << std::endl;
+	std::cout << "set_str2长度为:" << set_str2.size() << std::endl;
+
+	std::cout << "set_str能容纳的元素的最大限值为:" << set_str.max_size() << std::endl;
+	std::cout << "set_str2能容纳的元素的最大限值为:" << set_str2.max_size() << std::endl;
+
+	set_str2.insert("肖志赋");
+	m_str_iter = set_str2.find("肖志赋");    //查找键值为5的元素
+	if (m_str_iter != set_str2.end())    //找到
+		std::cout << *m_str_iter <<std::endl;
+	else            //未找到
+		std::cout << "未找到";
+
+	std::pair< std::set<std::string>::iterator, std::set<std::string>::iterator> pr;
+	pr = set_str2.equal_range("肖志赋");
+	std::cout << "肖志赋的前一个元素是 ：" << *pr.first << std::endl;
+	std::cout << "肖志赋的后一个元素是 ： " << *pr.second << std::endl;
+
+	std::cout << "set_str2反向输出为:" << std::endl;
+	for (m_str_iter1 = set_str2.rbegin(); m_str_iter1 != set_str2.rend(); m_str_iter++) {
+		std::cout << *m_str_iter << std::endl;
+	}
+
+	system("pause");
+	return 0;
+}
